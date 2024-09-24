13691da5aa22df0d89d32b6e9b4472e51fd923d5
SedAmir11 <amirhossein1384.kh@gmail.com>
2024-09-24 09:29:38 -0700

    add to basket and showing card views handleing

--- app_payment/views.py
+++ app_payment/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect , render
+from django.contrib.auth.decorators import login_required
+from app_payment.models import Basket, BasketItem
+from app_didikala.models import Product , Color
 
-# Create your views here.
+@login_required
+def add_to_basket(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    basket, created = Basket.objects.get_or_create(user=request.user)
+
+    color_id = request.POST.get('color')
+    color = None
+    if color_id:
+        color = get_object_or_404(Color, id=color_id)
+
+    basket_item, created = BasketItem.objects.get_or_create(basket=basket, product=product, color=color)
+    
+    if not created:
+        basket_item.count += 1 
+    else:
+        basket_item.count = 1 
+        
+
+    basket_item.save()
+    return redirect('cart')
+
+@login_required
+def view_basket(request):
+    basket, created = Basket.objects.get_or_create(user=request.user)
+    items = BasketItem.objects.filter(basket=basket)
+
+    if not items.exists():
+        return render(request, 'cart-empty.html')
+    
+    total_price = sum(item.product.price * item.count for item in items)
+    total_price_without_discount = sum(round(item.product.price/(1-(item.product.discount/100)))*item.count for item in items)
+    total_discount_price = sum(round((item.product.price/(1 - (item.product.discount/100))) - item.product.price)*item.count for item in items)
+    
+
+    return render(request, 'cart.html', {'items': items,
+                                        'total_price': total_price,
+                                        'total_price_without_discount' : total_price_without_discount,
+                                        'total_discount_price' : total_discount_price})
+
