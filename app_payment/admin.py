7c44f87cbd957f71845acdb404cc6761c5f873b9
SedAmir11 <amirhossein1384.kh@gmail.com>
2024-09-24 09:27:17 -0700

    change admin models

--- app_payment/admin.py
+++ app_payment/admin.py
@@ -1,23 +1,31 @@
 from django.contrib import admin
-from app_payment.models import transaction , basket_item , order , order_item , basket
+from app_payment.models import Transaction, BasketItem, Order, OrderItem, Basket
 
-
-@admin.register(transaction)
+@admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('ref_code', 'status', 'price')
+    search_fields = ('ref_code',)
+    list_filter = ('status',)
 
-@admin.register(basket_item)
-class basket_itemAdmin(admin.ModelAdmin):
-    pass
+@admin.register(BasketItem)
+class BasketItemAdmin(admin.ModelAdmin):
+    list_display = ('basket', 'product', 'count')
+    search_fields = ('product__name',)
+    list_filter = ('basket',)
 
-@admin.register(order)
-class orderAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'transaction')
+    search_fields = ('user__username',)
+    list_filter = ('transaction__status',)
 
-@admin.register(order_item)
-class order_itemAdmin(admin.ModelAdmin):
-    pass
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'count')
+    search_fields = ('product__name', 'order__user__username')
+    list_filter = ('order',)
 
-@admin.register(basket)
-class basketAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username',)
