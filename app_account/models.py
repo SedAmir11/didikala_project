50794a2b3002d01847fa4aff0ddecc8a97c8bba3
SedAmir11 <amirhossein1384.kh@gmail.com>
2024-10-01 14:26:42 -0700

    edit address model

--- app_account/models.py
+++ app_account/models.py
@@ -15,9 +15,14 @@ class UserProfile(models.Model):
         return f"{self.user.username} profile"
 
 
-class address(models.Model):
-    user =  models.OneToOneField(user , on_delete=models.CASCADE , null= True)
-    state = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    postal_code = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+class Address(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE, related_name='addresses')
+    full_name = models.CharField(max_length=255 , null = True)
+    phone_number = models.CharField(max_length=20 , null = True)
+    state = models.CharField(max_length=255 , null = True)
+    city = models.CharField(max_length=255 , null = True)
+    description = models.TextField()
+    postal_code = models.CharField(max_length=20 , null = True)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.city}, {self.state}"
