0af0d9308c9ee00a31874ad8bfeae631ef9f823a
SedAmir11 <amirhossein1384.kh@gmail.com>
2024-10-14 03:24:01 -0700

    commited

--- didikala_project/settings.py
+++ didikala_project/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'didikala_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
